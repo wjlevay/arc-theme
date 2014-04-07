f3795b402876e3eb0dc534d7eebf4f76242bd4b5
wjlevay <wjlevay@gmail.com>
2014-04-07 18:05:46 -0400

    gravatars
    created gravatars; added some comment styles

--- functions.php
+++ functions.php
@@ -71,6 +71,23 @@ You can change the names and dimensions to whatever
 you like. Enjoy!
 */
 
+add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
+
+function bones_custom_image_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'bones-thumb-713' => __('713px by 713px'),
+        'bones-thumb-220' => __('220px by 220px'),
+    ) );
+}
+
+/*
+The function above adds the ability to use the dropdown menu to select 
+the new images sizes you have just created from within the media manager 
+when you add media to your content blocks. If you add more image sizes, 
+duplicate one of the lines in the array and name it according to your 
+new image size.
+*/
+
 /************* ACTIVE SIDEBARS ********************/
 
 // Sidebars & Widgetizes Areas
@@ -240,14 +257,19 @@ function bones_comments( $comment, $args, $depth ) {
 					this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
 					echo get_avatar($comment,$size='32',$default='<path_to_url>' );
 				*/
-				?>
-				<?php // custom gravatar call ?>
+				
+				// custom gravatar call 
+				/*
 				<?php
 					// create variable
 					$bgauthemail = get_comment_author_email();
 				?>
 				<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=65" class="load-gravatar avatar photo" height="65" width="65" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
-				<?php // end custom gravatar call ?>
+				// end custom gravatar call 
+
+				*/
+				?>
+				<?php echo get_avatar( $comment, $size='64' ); ?>
 				<?php printf(__( '<cite class="fn">%s</cite>', 'bonestheme' ), get_comment_author_link()) ?>
 				<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
 				<?php edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ?>
@@ -403,5 +425,7 @@ function auto_featured_image() {
 
 // Use it temporarily to generate all featured images
 // add_action('the_post', 'auto_featured_image');
+
+
 
 ?>
